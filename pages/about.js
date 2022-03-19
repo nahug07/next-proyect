bb9070e58058d77a8d7313d283cab94c062ae4d2
Nahuel Grijalba <nahug07@gmail.com>
2022-03-19 18:18:11 -0300

    primeras pages

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <h3>About</h3>
+    </div>
+  )
+}
+
+export default about
---
+++ pages/product/[id].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const productItem = () => {
+  return (
+    <div> 
+       <h3>Esta es la página de los products</h3> 
+    </ div>
+  )
+}
+
+export default productItem;
